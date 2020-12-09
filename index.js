a40aa0594bffcc61e76e893367a610c6efc7fd9d
dereksimard28 <dereksimard28@outlook.com>
2020-12-09 10:50:50 -0500

    log

--- index.js
+++ index.js
@@ -91,9 +91,13 @@ var cartes = getCartes();
 // App setup
 var app = express();
 
+console.log("Service sur le port 3000");
+
 var server = app.listen(process.env.PORT || 3000, function () {
 	console.log("Service sur le port 3000");
 });
+
+console.log("Service sur le port 3000");
 
 // Static files
 app.use(express.static('public'));
